44fe985643544909f03ad45afc28922b0c7ba70b
Eren Alsan <115087362+ErenAlsan@users.noreply.github.com>
2023-05-31 17:27:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,13 @@ st.title('Credit Approval Prediction App')
 
 # Kullanıcıdan girişleri alma
 input_data = []
+input_labels = ['duration', 'euribor3m', 'age', 'nr. of employers', 'job', 'campaign', 'day_of_week', 'education']
 for i in range(8):
-    input_value = st.text_input(f'TAE Input {i+1}')
+    input_value = st.text_input(input_labels[i])
     input_data.append(input_value)
 
 # Tahmin yapma butonu
-if st.button('Can You get a loan?'):
+if st.button('Can You Get a Loan?'):
     # Burada tahmin yapma işlemini gerçekleştirebilirsiniz
     # Örnek olarak, giriş verilerini kullanarak tahmin sonucunu hesaplayabilirsiniz
     # prediction = model.predict(input_data)
